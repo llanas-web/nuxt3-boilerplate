72caf68c7152aad8ed60fe186446a8ba1d02f2f8
Boris MAURENCE <b.maurence@gmail.com>
2022-12-27 15:29:14 +0100

    chore: Upgrade to Nuxt 3 + tailwind

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-center h-full space-y-4 text-xl"
-  >
+  <div class="flex flex-col items-center justify-center h-full space-y-4 text-xl">
     <p>Here's a counter:</p>
     <div class="flex items-center justify-center">
       <button
@@ -23,7 +21,5 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
-
 const counter = ref(0);
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,28 @@
-import { defineNuxtConfig } from "nuxt3";
-
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/css/styles.css"],
-  build: {
-    postcss: {
-      postcssOptions: require("./postcss.config.js"),
+  css: ["~/assets/css/tailwind.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          "defineStore",
+          "storeToRefs", // import { defineStore } from 'pinia'
+          // automatically imports `defineStore` as `definePiniaStore`
+          ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+      },
+    ],
+  ],
+  imports: {
+    dirs: ["stores"],
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
   },
 });
